7103b3282d1b7eaf616337dc3664a41debc65132
hrach24 <hrachmuradyan2003@gmail.com>
2024-04-17 00:40:58 +0400

    add

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -3,9 +3,11 @@ import Header from "./header/Header";
 import Footer from "./footer/Footer";
 import { Outlet } from "react-router-dom";
 import classes from "./Layout.module.scss";
+import SvgSprite from "../svgSprite/svgSprite";
 const Layout = () => {
   return (
     <div className={classes.wrapper}>
+      <SvgSprite />
       <Header />
       <Outlet />
       <Footer />
--- src/components/liveStreamChecker/liveStreamList/LiveStreamList.jsx
+++ src/components/liveStreamChecker/liveStreamList/LiveStreamList.jsx
@@ -1,80 +1,214 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./LiveStream.module.scss";
 import { Link } from "react-router-dom";
-import ballImg from "../../../assets/images/ball.png";
-import tennisImg from "../../../assets/images/tennisBall.png";
-import basketBallImg from "../../../assets/images/basketBall.png";
-import hockeyImg from "../../../assets/images/iceHockey.png";
-import volleyBallImg from "../../../assets/images/valleyball.png";
-import tableTennisImg from "../../../assets/images/tabelTennis.png";
+import classNames from "classnames";
 const LiveStreamList = () => {
+  const [childDropDown, setChildDropDown] = useState(false);
+  const [parenthover, setParentHover] = useState(false);
   return (
     <ul className={classes.liveStreamList}>
       <li className={classes.liveStreamItem}>
         <Link to={"#"} alt={"#"} className={classes.liveStreamLink}>
-          <div className={classes.imgBlock}>
-            <div className={classes.imgContainer}>
-              <img src={ballImg} alt="" />
-            </div>
-          </div>
+          <svg className={classes.svgIcon}>
+            <use xlinkHref="#ball-football"></use>
+          </svg>
 
           <div className={classes.liveItemText}>Football</div>
         </Link>
       </li>
       <li className={classes.liveStreamItem}>
         <Link to={"#"} alt={"#"} className={classes.liveStreamLink}>
-          <div className={classes.imgBlock}>
-            <div className={classes.imgContainer}>
-              <img src={tennisImg} alt="" />
-            </div>
-          </div>
-
+          <svg className={classes.svgIcon}>
+            <use xlinkHref="#tennis"></use>
+          </svg>
           <div className={classes.liveItemText}>Tennis</div>
         </Link>
       </li>
       <li className={classes.liveStreamItem}>
         <Link to={"#"} alt={"#"} className={classes.liveStreamLink}>
-          <div className={classes.imgBlock}>
-            <div className={classes.imgContainer}>
-              <img src={basketBallImg} alt="" />
-            </div>
-          </div>
-
+          <svg className={classes.svgIcon}>
+            <use xlinkHref="#basketball"></use>
+          </svg>
           <div className={classes.liveItemText}>Basketball</div>
         </Link>
       </li>
       <li className={classes.liveStreamItem}>
         <Link to={"#"} alt={"#"} className={classes.liveStreamLink}>
-          <div className={classes.imgBlock}>
-            <div className={classes.imgContainer}>
-              <img src={hockeyImg} alt="" />
-            </div>
-          </div>
-
+          <svg className={classes.svgIcon}>
+            <use xlinkHref="#ice-hockey-puck"></use>
+          </svg>
           <div className={classes.liveItemText}>Basketball</div>
         </Link>
       </li>
       <li className={classes.liveStreamItem}>
         <Link to={"#"} alt={"#"} className={classes.liveStreamLink}>
-          <div className={classes.imgBlock}>
-            <div className={classes.imgContainer}>
-              <img src={volleyBallImg} alt="" />
-            </div>
-          </div>
-
+          <svg className={classes.svgIcon}>
+            <use xlinkHref="#volleyball-2"></use>
+          </svg>
           <div className={classes.liveItemText}>Volleyball</div>
         </Link>
       </li>
       <li className={classes.liveStreamItem}>
         <Link to={"#"} alt={"#"} className={classes.liveStreamLink}>
-          <div className={classes.imgBlock}>
-            <div className={classes.imgContainer}>
-              <img src={tableTennisImg} alt="" />
-            </div>
-          </div>
+          <svg className={classes.svgIcon}>
+            <use xlinkHref="#table-tennis-4"></use>
+          </svg>
 
           <div className={classes.liveItemText}>Table Tennis</div>
         </Link>
+      </li>
+
+      <li
+        className={classNames(
+          classes.liveStreamItem,
+          classes.liveStreamItemLines,
+        )}
+        onMouseEnter={() => setParentHover(true)}
+        onMouseLeave={() => setParentHover(false)}
+      >
+        <div className={classes.linesIconDown}>
+          <svg className={classes.svgIconLines}>
+            <use xlinkHref="#reorder-alt"></use>
+          </svg>
+          <svg className={classes.iconDown}>
+            <use xlinkHref="#arrow-down-338"></use>
+          </svg>
+        </div>
+        {parenthover ? (
+          <div
+            className={classes.dropDown}
+            onMouseEnter={() => setParentHover(true)}
+            onMouseLeave={() => setParentHover(false)}
+          >
+            <div className={classes.dropDownListSearchContainer}>
+              <input
+                type="text"
+                placeholder={"Search by match"}
+                className={classes.inputSearch}
+              />
+              <button className={classes.searchButton}>
+                <svg className={classes.searchSvg}>
+                  <use xlinkHref="#search"></use>
+                </svg>
+              </button>
+            </div>
+
+            <div className={classes.dropDownItemsContainer}>
+              <div className={classes.dropDownItemsContainerText}>TOP</div>
+              <ul className={classes.dropDowmListItems}>
+                <li className={classes.dropDownListItem}>
+                  {childDropDown ? (
+                    <div
+                      className={classes.footballDropDown}
+                      onMouseEnter={() => setChildDropDown(true)}
+                      onMouseLeave={() => setChildDropDown(false)}
+                    >
+                      <ul className={classes.footballDropDownList}>
+                        <li className={classes.dropDownListItem}>
+                          <Link
+                            to={"#"}
+                            className={classes.dropDownListItemLink}
+                          >
+                            England
+                          </Link>
+                        </li>
+                        <li className={classes.dropDownListItem}>
+                          <Link
+                            to={"#"}
+                            className={classes.dropDownListItemLink}
+                          >
+                            Scotland
+                          </Link>
+                        </li>
+                        <li className={classes.dropDownListItem}>
+                          <Link
+                            to={"#"}
+                            className={classes.dropDownListItemLink}
+                          >
+                            Iceland
+                          </Link>
+                        </li>
+                      </ul>
+                    </div>
+                  ) : null}
+
+                  <Link
+                    to={"#"}
+                    className={classes.dropDownListItemLink}
+                    onMouseEnter={() => setChildDropDown(!childDropDown)}
+                    onMouseLeave={() => setChildDropDown(false)}
+                  >
+                    <svg className={classes.dropDownListItemSvg}>
+                      <use xlinkHref="#ball-football"></use>
+                    </svg>
+                    Football
+                  </Link>
+                </li>
+                <li className={classes.dropDownListItem}>
+                  <Link to={"#"} className={classes.dropDownListItemLink}>
+                    <svg className={classes.dropDownListItemSvg}>
+                      <use xlinkHref="#basketball"></use>
+                    </svg>
+                    Basketball
+                  </Link>
+                </li>
+                <li className={classes.dropDownListItem}>
+                  <Link to={"#"} className={classes.dropDownListItemLink}>
+                    <svg className={classes.dropDownListItemSvg}>
+                      <use xlinkHref="#tennis"></use>
+                    </svg>
+                    Tennis
+                  </Link>
+                </li>
+                <li className={classes.dropDownListItem}>
+                  <Link to={"#"} className={classes.dropDownListItemLink}>
+                    <svg className={classes.dropDownListItemSvg}>
+                      <use xlinkHref="#ice-hockey-puck"></use>
+                    </svg>
+                    Ice Hockey
+                  </Link>
+                </li>
+              </ul>
+              <div className={classes.dropDownItemsContainerText}>
+                CATEGORIES FROM A TO Z
+              </div>
+              <ul className={classes.dropDowmListItems}>
+                <li className={classes.dropDownListItem}>
+                  <Link to={"#"} className={classes.dropDownListItemLink}>
+                    <svg className={classes.dropDownListItemSvg}>
+                      <use xlinkHref="#ball-football"></use>
+                    </svg>
+                    Football
+                  </Link>
+                </li>
+                <li className={classes.dropDownListItem}>
+                  <Link to={"#"} className={classes.dropDownListItemLink}>
+                    <svg className={classes.dropDownListItemSvg}>
+                      <use xlinkHref="#basketball"></use>
+                    </svg>
+                    Basketball
+                  </Link>
+                </li>
+                <li className={classes.dropDownListItem}>
+                  <Link to={"#"} className={classes.dropDownListItemLink}>
+                    <svg className={classes.dropDownListItemSvg}>
+                      <use xlinkHref="#tennis"></use>
+                    </svg>
+                    Tennis
+                  </Link>
+                </li>
+                <li className={classes.dropDownListItem}>
+                  <Link to={"#"} className={classes.dropDownListItemLink}>
+                    <svg className={classes.dropDownListItemSvg}>
+                      <use xlinkHref="#ice-hockey-puck"></use>
+                    </svg>
+                    Ice Hockey
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        ) : null}
       </li>
     </ul>
   );
