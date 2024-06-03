755b95dada8cf85d618668488e16a3b74089b71d
Hrach Muradyan <hrachmuradyan@joytech.am>
2024-06-03 11:04:59 +0400

    add

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import classes from "./Home.module.scss";
 import LeftAsideHeader from "../../components/layout/leftAsideHeader/LeftAsideHeader";
 import RightAsideHeader from "../../components/layout/rightAsideHeader/RightAsideHeader";
@@ -6,8 +6,18 @@ import Content from "../../components/content/Content";
 import { useMediaQuery } from "react-responsive";
 import { ClickContext } from "../../util/Context";
 import { values } from "../../util/ContextFunctions";
-
+import axios from "axios";
 const Home = () => {
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   const hideLeftBar = useMediaQuery({ query: "(max-width: 1367px)" });
   return (
     <main className={classes.main}>
